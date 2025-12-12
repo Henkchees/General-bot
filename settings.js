4bad90ade440f3fb2b4a364d1110c9fe65c014d0
Henkchees <henkk514@gmail.com>
2025-12-13 00:19:13 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@
 // Update the values below to personalise the site without touching HTML.
 window.settings = {
     botName: 'Nextgen',
-    botTagline: 'NextGen is a advanced bot that allaws you to kick/ban/clear/time-out',
+    botTagline: 'NextGen is a powerful moderation bot that keeps your community safe and organized. With features like kicking, banning, timeouts, and clearing chats, NextGen ensures order, control, and a pleasant environment for everyone.',
     botDescription: 'General Bot',
     theme: 'purple',
     inviteLink: 'https://discord.com/oauth2/authorize?client_id=1444071063817289819&permissions=1689402032205046&integration_type=0&scope=bot',
